53fb636fece326e339936987b656430016b13aa7
Eddie Koranek <eddie.koranek.12@gmail.com>
2022-03-14 19:21:55 -0500

    Initial Commit

---
+++ Totally Not Wordle/Totally_Not_WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Totally_Not_WordleApp.swift
+//  Totally Not Wordle
+//
+//  Created by Eddie Koranek on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Totally_Not_WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
